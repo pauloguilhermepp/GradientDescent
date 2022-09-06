1c751a8102fca63cdae737bad35b0f77ca1fae41
Paulo Guilherme <pauloguilherme@usp.br>
2022-09-05 22:28:06 -0300

    Add files to represent the Gradient Descent class

---
+++ src/GradientDescent/GradientDescent.cpp
@@ -0,0 +1,81 @@
+#include "GradientDescent.hpp"
+
+GradientDescent::GradientDescent(int NumVar, int NumIt,
+                        long double Alpha, long double Dx,
+                        long double MinValue, long double MaxValue,
+                        long double (*EvaluationFunc)(std::vector<long double>)){
+                            numVar = NumVar;
+                            numIt = NumIt;
+                            alpha = Alpha;
+                            dx = Dx;
+                            minValue = MinValue;
+                            maxValue = MaxValue;
+                            evaluationFunc = EvaluationFunc;
+}
+
+std::vector<long double> GradientDescent::gradient(){
+    long double newValue;
+    std::vector<long double> gradient, tempSolution;
+    tempSolution = solution;
+ 
+    for(int i = 0; i < numVar; i++){
+        tempSolution[i] = solution[i] + dx;
+        newValue = (evaluationFunc(tempSolution) - evaluationFunc(solution)) / dx;
+        gradient.push_back(newValue);
+        tempSolution[i] = solution[i];
+    }
+
+    return gradient;
+}
+
+void GradientDescent::showSolution(){
+    std::cout << "Solution: ";
+    for(int i = 0; i < numVar; i++){
+        std::cout << solution[i] << " ";
+    }
+
+    std::cout << std::endl;
+    std::cout << "EvaluationFunctionValue: " << evaluationFunc(solution) << std::endl;
+}
+
+void GradientDescent::run(){
+    long double newValue;
+    std::vector<long double> dSolution;
+
+    solution.reserve(numVar);
+    solution = uniformVectorRandomNum(numVar);
+
+    for(int i = 0; i < numIt; i++){
+        dSolution = gradient();
+
+        for(int j = 0; j < numVar; j++){
+            newValue = solution[j] - alpha * dSolution[j];
+
+            if(newValue > maxValue){
+                newValue = maxValue;
+            }else if(newValue < minValue){
+                newValue = minValue;
+            }
+
+            solution[j] = newValue;
+        }
+
+    }
+
+    showSolution();
+}
+
+long double GradientDescent::uniformRandomNum(long double MinValue, long double MaxValue){
+    double f = (double)rand() / RAND_MAX;
+    return (long double)(MinValue + f * (MaxValue - MinValue));
+}
+
+std::vector<long double> GradientDescent::uniformVectorRandomNum(int size){
+    std::vector<long double> randomNums;
+
+    for(int i = 0; i < size; i++){
+        randomNums.push_back(uniformRandomNum(minValue, maxValue));
+    }
+
+    return randomNums;
+}
---
+++ src/GradientDescent/GradientDescent.hpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <iostream>
+#include <functional> 
+#include<algorithm>
+
+#ifndef GRADIENT_DESCENT_HPP
+#define GRADIENT_DESCENT_HPP
+class GradientDescent{
+    public:
+        GradientDescent(int numVar, int numIt, long double alpha, long double dx,
+                        long double minValue, long double maxValue,
+                        long double (*evaluationFunc)(std::vector<long double>));
+        void run();
+
+    //private:
+        int numVar, numIt;
+        std::vector<long double> solution;
+        long double alpha, dx, minValue, maxValue;
+        long double (*evaluationFunc)(std::vector<long double>);
+
+        std::vector<long double> gradient();
+        void showSolution();
+
+        long double uniformRandomNum(long double MinValue, long double MaxValue);
+        std::vector<long double> uniformVectorRandomNum(int size);
+};
+#endif
